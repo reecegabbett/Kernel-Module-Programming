a5ffd34813a3404f3b58ab72a8b00faede98953f
finley-talley <73396510+finley-talley@users.noreply.github.com>
2022-02-20 23:45:11 -0500

    added another folder to gitignore and added some headers to timer

---
+++ part2/my_timer.c
@@ -0,0 +1,8 @@
+// #include <linux/init.h>
+// #include <linux/module.h>
+// #include <asm/semaphore.h>
+// #include <linux/list.h>
+// #include <linux/string.h>
+
+#include<linux/kernel.h>
+#include<linux/time.h>
