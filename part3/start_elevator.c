5f594fd1aa3bdecb409aea4ae0cc836a191ca9a1
Reece Gabbett <81770210+reecegabbett@users.noreply.github.com>
2022-03-12 15:04:35 -0500

    Create start_elevator.c

---
+++ part3/start_elevator.c
@@ -0,0 +1,16 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+
+long (*STUB_start_elevator)(void) = NULL;
+EXPORT_SYMBOL(STUB_start_elevator);
+
+SYSCALL_DEFINE0(start_elevator) {
+    	if (STUB_start_elevator != NULL)
+            	return STUB_start_elevator();
+    	else
+            	return -ENOSYS;
+}
+
+
